a2ccf758a54ba80aefdebed261af62953c886724
Lyric <vertiipavel0684@gmail.com>
2025-09-29 09:54:58 +0300

    Описал конфигурационный файл

---
+++ src/fastapi_voting/app/core/settings.py
@@ -0,0 +1,29 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+from src.fastapi_voting.app.core.utils import get_root_path
+
+
+class Settings(BaseSettings):
+
+    # --- Конфигурация приложения ---
+    SECRET_KEY: str
+    APP_HOST: str
+    APP_PORT: int
+
+    # --- Конфигурация данных БД ---
+    DB_HOST: str
+    DB_PORT: str
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+
+    class Config:
+        env_file = get_root_path() / ".env"
+
+    def get_db_url(self) -> str:
+        return f"mysql+asyncmy/{self.DB_USER}@{self.DB_PASSWORD}//{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
