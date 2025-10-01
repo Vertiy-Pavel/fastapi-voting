ba499d36236266406979213532882e749879e9b6
Lyric <vertiipavel0684@gmail.com>
2025-10-01 14:16:44 +0300

    Основа: Новая зависимость и расширение деталей приложения

--- src/fastapi_voting/app/main.py
+++ src/fastapi_voting/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from logging import getLogger
 
+from src.fastapi_voting.app.api.user_api import user_router
+
+
 # --- Создание логера и приложения FastApi ---
 logger = getLogger('fastapi-voting')
 
-fastapi_app = FastAPI()
+fastapi_app = FastAPI(
+    title='FastAPI-Voting',
+    version='1.0',
+    description='FastAPI-Voting',
+    docs_url='/docs',
+    redoc_url='/redoc',
+)
+v1_url_prefix = '/api/v1'
+
+# --- Регистрация обработчиков маршрутов ---
+fastapi_app.include_router(router=user_router, prefix=v1_url_prefix)
