7a7621ea089f600c5c8cbb45e8ef6eab2156e8b3
Lyric <vertiipavel0684@gmail.com>
2025-10-02 09:48:33 +0300

    Services: Описал проверку на уникальность записи по полям почты и телефона. Изменил шаблон ответа от сервиса

--- src/fastapi_voting/app/services/user_service.py
+++ src/fastapi_voting/app/services/user_service.py
@@ -1,6 +1,8 @@
+from fastapi import HTTPException
+
 from src.fastapi_voting.app.repositories.user_repo import UserRepo
 
-from src.fastapi_voting.app.schemas.user_schema import InputUserSchema
+from src.fastapi_voting.app.schemas.user_schema import InputCreateUserSchema
 
 
 class UserService:
@@ -8,11 +10,24 @@ class UserService:
         self.user_repo = user_repo
 
 
-    async def register(self, data: InputUserSchema):
+    async def register(self, data: InputCreateUserSchema):
         """Отвечает за регистрацию нового пользователя"""
-
+        # TODO: Валидация почты и телефона
         # --- Инициализация и извлечение первичных данных ---
         user_data = data.model_dump()
 
+        # --- Проверка на уникальность пользователя ---
+        user_by_phone = await self.user_repo.get_by_item(column=self.user_repo.model.phone, item=user_data["phone"])
+        user_by_email = await self.user_repo.get_by_item(column=self.user_repo.model.email, item=user_data["email"])
+
+        if user_by_phone:
+            raise HTTPException(status_code=409, detail="Пользователь с таким номером телефона уже существует")
+
+        if user_by_email:
+            raise HTTPException(status_code=409, detail="Пользователь с таким адресом электронной почты уже существует")
+
         # --- Регистрация пользователя ---
-        await self.user_repo.add_user(user_data)
+        result = await self.user_repo.add_user(user_data)
+
+        # --- Формирование ответа ---
+        return result
