55b9c5632a53de00cd35cacc1a8d09c4dca25bf9
Lyric <vertiipavel0684@gmail.com>
2025-10-02 16:01:56 +0300

    API, Utils: - Дополнил АПИ авторизации на лад контроля сессий - Описал утилиту для генерации пары токенов

--- src/fastapi_voting/app/api/user_api.py
+++ src/fastapi_voting/app/api/user_api.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends
-from fastapi import Body
+from fastapi import APIRouter
 
+from fastapi_voting.app.core.utils import create_tokens
 from src.fastapi_voting.app.di.annotations import UserServiceAnnotation
-from src.fastapi_voting.app.services.user_service import UserService
 
 from src.fastapi_voting.app.schemas.user_schema import InputCreateUserSchema, OutputCreateUserSchema
-from src.fastapi_voting.app.schemas.user_schema import InputLoginUserSchema, OutputLoginUserSchema
+from src.fastapi_voting.app.schemas.user_schema import InputLoginUserSchema, LoginResponseSchema
+
 
 # --- Конфигурация обработчика маршрутов, связанных с пользователями ---
 user_router = APIRouter(
@@ -18,16 +18,25 @@ async def user_register(
         data: InputCreateUserSchema,
         user_service: UserServiceAnnotation
 ):
-    # TODO: Контроль сессий с помощью JWT
     registered_user = await user_service.register(data)
     return registered_user
 
 
-@user_router.post("/login", response_model=OutputLoginUserSchema)
+@user_router.post("/login", response_model=LoginResponseSchema)
 async def user_login(
         data: InputLoginUserSchema,
         user_service: UserServiceAnnotation
 ):
     # TODO: Контроль сессий с помощью JWT
+    # --- Инициализация данных ---
+    remember_flag: bool = data.model_dump().get("remember_me", False)
+
+    # --- Работа сервиса ---
     logined_user = await user_service.login(data)
-    return logined_user
+
+    # --- Формирование ответа сервера ---
+    response = {
+        "user": logined_user,
+        "tokens": create_tokens(user_id=logined_user.id, refresh=remember_flag)
+    }
+    return response
--- src/fastapi_voting/app/core/utils.py
+++ src/fastapi_voting/app/core/utils.py
@@ -1,5 +1,29 @@
 from pathlib import Path
 
+from jose import jwt
 
+
+# --- Вспомогательные утилиты ---
 def get_root_path() -> Path:
+    """Формирование маршрута корня проекта в ОС"""
+
     return Path(__file__).parent.parent.parent.parent.parent
+
+
+def create_tokens(user_id: int, refresh: bool) -> dict:
+    """Формирование JWT-токенов"""
+
+    from src.fastapi_voting.app.core.settings import Settings
+    settings = Settings()
+
+    access_payload = {"user_id": user_id, "token_type": "access"}
+    access_token: str = jwt.encode(access_payload, settings.JWT_SECRET_KEY, algorithm="HS256")
+
+    if refresh:
+        refresh_payload = {"user_id": user_id, "token_type": "refresh"}
+        refresh_token: str = jwt.encode(refresh_payload, settings.JWT_SECRET_KEY, algorithm="HS256")
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token if refresh else None,
+    }
