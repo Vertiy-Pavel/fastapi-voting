1729e6a1c5b7953e7313c5860a6df3167c8b181f
Lyric <vertiipavel0684@gmail.com>
2025-10-02 09:44:42 +0300

    Repositories: Реализовал универсальные методы парсинга записей с бд: по первичному ключу, по произвольному значению

--- src/fastapi_voting/app/repositories/base_repo.py
+++ src/fastapi_voting/app/repositories/base_repo.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select, text
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -10,5 +12,11 @@ class Base:
     async def get_all(self):
         pass
 
-    async def get_by_id(self):
-        pass
+    async def get_by_id(self, id: int):
+        return self.session.get(self.model, id)
+
+    async def get_by_item(self, column, item: any):
+        query = select(self.model).where(column == item)
+        result = await self.session.execute(query)
+        return result.scalars().first()
+
