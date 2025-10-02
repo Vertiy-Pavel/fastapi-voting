aa29b2707c2d945b788bdf2bcf65aa20f0ac8b7c
Lyric <vertiipavel0684@gmail.com>
2025-10-02 16:00:09 +0300

    Dependencies, Annotations: - Описал зависимость для дешифровки JWT-токенов - Описал аннотацию зависимости для дешифровки JWT-токенов

--- src/fastapi_voting/app/di/annotations.py
+++ src/fastapi_voting/app/di/annotations.py
@@ -1,11 +1,14 @@
 from fastapi import Depends
 
-from typing import Annotated
+from typing import Annotated, Required
 
 from src.fastapi_voting.app.services.user_service import UserService
 
-from src.fastapi_voting.app.di.dependencies import get_user_service
+from src.fastapi_voting.app.di.dependencies import get_user_service, get_encode_jwt
 
 
-# --- Аннотации ---
+# --- Аннотации для сервисов---
 UserServiceAnnotation = Annotated[UserService, Depends(get_user_service)]
+
+# --- Первичные аннотации ---
+RequiredJWTAnnotation = Annotated[int, Depends(get_encode_jwt)]
--- src/fastapi_voting/app/di/dependencies.py
+++ src/fastapi_voting/app/di/dependencies.py
@@ -1,4 +1,10 @@
-from fastapi import Depends, HTTPException, status
+import logging
+
+from jose import jwt, JWTError
+
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,12 +14,37 @@ from src.fastapi_voting.app.repositories.user_repo import UserRepo
 
 from src.fastapi_voting.app.services.user_service import UserService
 
+from src.fastapi_voting.app.core.settings import Settings
+
+
+# --- Определение первичных конфигураций ---
+logger = logging.getLogger("fastapi-voting")
+settings = Settings()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
 
 # --- Создание сессии с MySQL --
 async def get_db() -> AsyncSession:
         async with AsyncSessionLocal() as session:
             yield session
             await session.close()
+
+
+# --- Работа с JWT ---
+async def get_encode_jwt(token: str = Depends(oauth2_scheme)) -> int:
+    """Декодирует токен, извлекая и возвращая id пользователя"""
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+        user_id = payload.get("user_id")
+
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Некорректный JWT-токен")
+
+    except JWTError as e:
+        logger.error(e)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Некорректный JWT-токен", headers={"WWW-Authenticate": "Bearer"})
+
+    return user_id
 
 
 # --- Зависимости-репозитории ---
