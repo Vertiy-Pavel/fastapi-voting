24dd6264340fd79d06e0bf5891ebd772c8331a6a
Lyric <vertiipavel0684@gmail.com>
2025-10-07 13:38:15 +0300

    Dummy: Рекурсивное построение дерева связей для отделов

--- src/fastapi_voting/app/poetry_cli/dummy_db/department_dummy.py
+++ src/fastapi_voting/app/poetry_cli/dummy_db/department_dummy.py
@@ -20,65 +20,118 @@ random = random.Random()
 async def get_fake_departments(session: AsyncSession, users: list[User]) -> tuple:
     """Генерирует список отделов. Кол-во отделов напрямую зависит от кол-ва шефов по паритету."""
 
-    async def set_dep_users(head_user: User, emp_users: list[User], dep: Department) -> list[User]:
+    async def get_tree_departments(root_dep: Department, employeers: list[User], chiefs: set[User]):
+        # TODO: Реализовать рекурсивную генерацию дерева связей дочерних отделов
+        """
+        Рекурсивно генерирует дерево связей.
+        Args:
+            root_dep (Department): Определяет родительский отдел:
+            employeers (list[User]): Определяет сотрудников, которых планируется связать с отделом.
+            chiefs (list[User]): Определяет шефов, которых планируется связать с отделом.
+        """
+
+        # --- Рекурсия прекращается по факту исчерпания доступных начальников ---
+        if not chiefs:
+            return None
+
+        # --- Выборка сотрудников ---
+        l_head_root_depart_choice = chiefs.pop()
+        l_employee_root_depart_sample = random.sample(employeers, random.randint(1, len(employeers)))
+
+        # --- Создание родительского отдела ---
+        l_department = Department(
+            name=faker.name(),
+            description=faker.text(),
+            location=faker.city(),
+
+            parent_id=root_dep.id,
+            parent=root_dep,
+
+            head_of_department=l_head_root_depart_choice,
+            head_of_department_id=l_head_root_depart_choice.id,
+
+            users=l_employee_root_depart_sample,
+        )
+        daughter_dep = await get_tree_departments(l_department, l_employee_root_depart_sample, chiefs)
+
+        if daughter_dep:
+            l_department.children.append(daughter_dep)
+
+        return l_department
+
+
+    def set_dep_users(head_user: User, emp_users: list[User], dep: Department) -> None:
         """Связывает пользователей с указанным отделом"""
         if not head_user.departments:
             head_user.departments.append(dep)
 
-        dep_users = [head_user]
         head_user.manage_department = dep
 
         for employee in emp_users:
             employee.departments.append(dep)
-            dep_users.append(employee)
-
-        return dep_users
+            transactions.add(employee)
 
 
     # --- Инициализация копии списка пользователей, делегирование надвое: шефы, работники ---
     users = users.copy()
-    chief_users =  []
-    employee_users =  []
+    chief_users = set()
+    employee_users = set()
 
     for user in users:
         if user.role.value == RolesEnum.CHIEF.value:
-            chief_users.append(user)
+            chief_users.add(user)
             continue
-        employee_users.append(user)
+        employee_users.add(user)
 
     # --- Вспомогательные данные ---
-    departments = []
-    transactions = []
+    departments = set()
+    transactions = set()
 
     # --- Генерация отделов ---
-    for _ in range(len(chief_users)):
-        random_chief_id = random.randint(0, len(chief_users) - 1)
-        head_depart_choice = chief_users.pop(random_chief_id)
-        employee_choice = random.sample(employee_users, 12)
+    for _ in range(3):
 
-        all_users = [head_depart_choice, *employee_choice]
+        # Выборка данных про глав отделов
+        head_root_depart_choice = chief_users.pop()
 
+        if len(chief_users) <= 3:
+            head_daughter_depart_sample = set()
+        else:
+            head_daughter_depart_sample = set(random.sample(list(chief_users), random.randint(1, len(chief_users)-3)))
+
+        chief_users = chief_users - head_daughter_depart_sample
+
+        # Выборка данных про сотрудников отделов
+        employee_root_depart_sample = random.sample(list(employee_users), random.randint(1, 7))
+        employee_daughter_depart_sample = random.sample(list(employee_users), random.randint(0, 25))
+
+        # --- Инициализация корневого отдела ---
         department = Department(
-            name = faker.name(),
-            description = faker.text(),
-            location = faker.city(),
+                name = faker.name(),
+                description = faker.text(),
+                location = faker.city(),
 
-            parent_id = None, # TODO: Реализовать рекурсивную генерацию дерева связей дочерних отделов
-                              # TODO: parent
-                              # TODO: children
-            head_of_department = head_depart_choice,
-            head_of_department_id = head_depart_choice.id,
+                parent_id = None,
+                parent = None,
 
-            users = all_users,
+                head_of_department = head_root_depart_choice,
+                head_of_department_id = head_root_depart_choice.id,
+
+                users = [head_root_depart_choice, *employee_root_depart_sample]
+            )
+
+        # --- Рекурсивная генерация дерева дочерних отделов ---
+        department.children.append(
+            await get_tree_departments(
+                department,
+                employee_daughter_depart_sample,
+                head_daughter_depart_sample)
         )
-        departments.append(department)
+        departments.add(department)
+        transactions.add(department)
 
-        # --- Манипуляции со связанными ОРМ-моделями ---
-        update_users = await set_dep_users(head_depart_choice, employee_choice, department)
-        transactions.extend(update_users)
+        # --- Вторичные манипуляции со связанными ОРМ-моделями ---
+        set_dep_users(head_root_depart_choice, employee_root_depart_sample, department)
 
-    transactions.extend(departments)
+    # --- Фиксация результирующего состояния транзакции ---
     session.add_all(transactions)
-
     return session, departments
-
