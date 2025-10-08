1c9bbc1d27bd7d6bc15eaf4f9192f2a0d10b583c
Lyric <vertiipavel0684@gmail.com>
2025-10-08 09:51:59 +0300

    Services: - Описал сервис для работы с отделами - Описал метод для выгрузки всех отделов

---
+++ src/fastapi_voting/app/services/department_service.py
@@ -0,0 +1,19 @@
+import logging
+
+from src.fastapi_voting.app.repositories.department_repo import DepartmentRepo
+
+from src.fastapi_voting.app.schemas.department_schema import ResponseAllDepartmentsSchema
+
+
+# --- Инициализация первичных данных ---
+logger = logging.getLogger("fastapi-voting")
+
+class DepartmentService:
+
+    def __init__(self, department_repo: DepartmentRepo):
+        self.department_repo = department_repo
+
+
+    async def get_all_departments(self):
+        departments = await self.department_repo.get_all_departments()
+        return ResponseAllDepartmentsSchema.model_validate(departments)
