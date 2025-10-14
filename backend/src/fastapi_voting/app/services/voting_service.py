842d8a9ebfccc2133905180941cc23649bd5adb4
Lyric <vertiipavel0684@gmail.com>
2025-10-14 09:52:59 +0300

    Services: Описал метод для мягкого удаления голосования

--- backend/src/fastapi_voting/app/services/voting_service.py
+++ backend/src/fastapi_voting/app/services/voting_service.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from src.fastapi_voting.app.schemas.voting_schema import InputCreateVotingSchema
 
 from src.fastapi_voting.app.repositories.voting_repo import VotingRepo
@@ -12,6 +14,7 @@ class VotingService:
 
 
     async def create_voting(self, voting_data: InputCreateVotingSchema) -> Voting:
+
         # --- Первичные данные ---
         data = voting_data.model_dump()
 
@@ -20,3 +23,17 @@ class VotingService:
 
         # --- Ответ сервиса ---
         return voting
+
+
+    async def delete_voting(self, voting_id: int) -> bool:
+
+        # --- Проверка на существование записи ---
+        voting = await self.voting_repo.get_by_id(voting_id)
+        if (voting is None) or (voting.deleted):
+            raise HTTPException(status_code=404, detail="Голосования не существует")
+
+        # --- Работа репозитория ----
+        await self.voting_repo.delete(voting)
+
+        # --- Ответ сервиса ---
+        return True
