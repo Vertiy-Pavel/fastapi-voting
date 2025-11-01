96f396c4136ebc73ee068cea420781e75ae586d1
Lyric <vertiipavel0684@gmail.com>
2025-11-01 12:41:01 +0300

    Annotations: Аннотация для сервса токенов

--- backend/src/fastapi_voting/app/di/annotations.py
+++ backend/src/fastapi_voting/app/di/annotations.py
@@ -1,9 +1,10 @@
-import redis
+from redis.asyncio import Redis
 
 from fastapi import Depends
 
 from typing import Annotated
 
+from src.fastapi_voting.app.services.token_service import TokenService
 from src.fastapi_voting.app.services.user_service import UserService
 from src.fastapi_voting.app.services.department_service import DepartmentService
 from src.fastapi_voting.app.services.voting_service import VotingService
@@ -15,7 +16,8 @@ from src.fastapi_voting.app.di.dependencies.auth_di import (
 from src.fastapi_voting.app.di.dependencies.services_di import (
     get_user_service,
     get_department_service,
-    get_voting_service
+    get_voting_service,
+    get_token_service,
 )
 from src.fastapi_voting.app.di.dependencies.databases_di import (
     get_redis
@@ -28,10 +30,12 @@ DepartmentServiceAnnotation = Annotated[DepartmentService, Depends(get_departmen
 VotingServiceAnnotation = Annotated[VotingService, Depends(get_voting_service)]
 
 # --- Аннотации для токенов ---
+TokenServiceAnnotation = Annotated[TokenService, Depends(get_token_service)]
+
 AccessRequiredAnnotation = Annotated[AuthTokenRequired, Depends(AuthTokenRequired("access_token"))]
 RefreshRequiredAnnotation = Annotated[AuthTokenRequired, Depends(AuthTokenRequired("refresh_token"))]
 CSRFValidAnnotation = Annotated[csrf_valid, Depends(csrf_valid)]
 
 # --- Аннотации для Redis ---
-RedisClientAnnotation = Annotated[redis.Redis, Depends(get_redis)]
+RedisClientAnnotation = Annotated[Redis, Depends(get_redis)]
 
