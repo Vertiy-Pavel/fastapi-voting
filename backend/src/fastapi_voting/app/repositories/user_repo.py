36a928b1bfa47b41000305300313f70af76a8c06
Lyric <vertiipavel0684@gmail.com>
2025-12-18 10:49:57 +0300

    Repositories: Расширил метод для создания записи о пользователе с учётом привязки к отделам

--- backend/src/fastapi_voting/app/repositories/user_repo.py
+++ backend/src/fastapi_voting/app/repositories/user_repo.py
@@ -2,10 +2,9 @@ from sqlalchemy import select, update
 
 
 from src.fastapi_voting.app.repositories.base_repo import Base
+from src.fastapi_voting.app.repositories.department_repo import Department
 
 from src.fastapi_voting.app.models.user import User
-
-from src.fastapi_voting.app.schemas.user_schema import InputCreateUserSchema
 
 
 class UserRepo(Base):
@@ -14,7 +13,18 @@ class UserRepo(Base):
         super().__init__(User, session)
 
     async def add_user(self, data: dict) -> User:
+        """Вносит запись о новом пользователе"""
+
+        # Работа с первичными данными
         password = data.pop("password")
+        dep_ids = data.pop("departments")
+
+        # Выборка связанных отделов
+        deps_query = select(Department).where(Department.id.in_(dep_ids))
+        deps = await self.session.execute(deps_query)
+        data["departments"] = deps.scalars().all()
+
+        # Формирование записи о пользователе
         user = self.model(**data)
         user.set_hash_password(password)
 
